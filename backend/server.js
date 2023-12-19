4d4337f78029d04a1bb4f7c9cd439851ade951ef
Jayabrata Pramanik <jayabrata180402@gmail.com>
2023-12-19 22:19:09 +0530

    added vercel.json

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,13 @@ const port = 5000;
 const jwtKey = process.env.JWT_KEY || "default_secret_key";
 // Middleware
 app.use(express.json());
-app.use(cors());
+app.use(
+  cors({
+    origin: [],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
 
 // Connect to MongoDB
 mongoose
